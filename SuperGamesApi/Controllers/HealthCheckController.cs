32c93cc126c0a032ad71640a6b169e142c3a9a11
martinrodfl <martinrodfl@gmail.com>
2024-04-24 16:12:41 -0300

    Added HealthCheckController

---
+++ SuperGamesApi/Controllers/HealthCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperGamesApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public string HealthCheck()
+        {
+            return "Server OK";
+        }
+    }
+}
